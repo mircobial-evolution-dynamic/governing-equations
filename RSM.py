b5df11b1a796d6ee3c5a4f77c17e319ccc85b269
onionpork <yiruic@g.clemson.edu>
2020-02-22 16:59:06 -0500

    Update RSM.py

--- RSM.py
+++ RSM.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import integrate
+import csv
+from scipy import optimize
 from sklearn.linear_model import Lasso
 from sklearn.model_selection import train_test_split
 
@@ -34,34 +36,50 @@ def data_aug(sol_):
         for j in range(n, 2*n):
             sol_ = np.vstack((sol_, sol_[i]*sol_[j]))
     return sol_
-print('sep')
 p1 = data_aug(sol.y)
 exp_data = np.diff(sol.y, axis=1) /dt
 p1 = p1[:, 1:]
 
+coeff_ = np.dot(np.linalg.pinv(p1.T), exp_data.T)
+est = np.dot(p1.T, coeff_)
+plt.plot(est[:,2], lw=4)
+plt.plot(exp_data[2,:].T)
+plt.show()
+print(est)
+
+csvfile = "./output.csv"
+with open(csvfile, "w") as output:
+    writer = csv.writer(output, lineterminator='\n')
+    writer.writerows(coeff_)
+
 # print(exp_data.shape)
 # print(p1.shape)
 
-coeff_set = []
-for i in range(len(exp_data)):
-    X_train, X_test, y_train, y_test = train_test_split(p1.T, np.expand_dims(exp_data[0,:], axis=0).T, test_size=0.1, random_state= 10)
-    las = Lasso(alpha=1, max_iter=10**5, tol=0.001)
-    las.fit(X_train, y_train)
-    y_est = las.predict(X_test)
-    coeff_set.append(las.coef_)
-
-# print(p1[0,:].T)
-# print(coeff_set[0])
-# print(np.dot(p1[0,:].T, coeff_set[0]))
-# for i in range(len(coeff_set)):
-#     est = np.dot(p1.T, coeff_set[i].T)
-#     plt.plot(est)
-#     plt.show()
-
-
-# X_train, X_test, y_train, y_test = train_test_split(p1.T, exp_data.T, test_size=0.34, random_state= 10)
-# las = Lasso()
-# las.fit(X_train, y_test)
-# y_est = las.predict(X_test)
+# def fun(coeff_, p1, exp_data):
+#     return np.dot(p1.T, coeff_) - exp_data.T
 #
-# print(las.coef_)
+# coeff_guess = np.array([np.random.rand(18)]).T
+# print(coeff_guess.shape)
+# res_log = optimize.least_squares(fun, coeff_guess, loss='cauchy', f_scale=0.1,
+#                                  args=(p1, exp_data))
+# # coeff_set = []
+# # for i in range(len(exp_data)):
+# #     X_train, X_test, y_train, y_test = train_test_split(p1.T, np.expand_dims(exp_data[i,:], axis=0).T, test_size=0.1)
+# #     las = Lasso(alpha=1, max_iter=10**5, tol=0.001)
+# #     las.fit(X_train, y_train)
+# #     y_est = las.predict(X_test)
+# #     coeff_set.append(las.coef_)
+#
+#
+# # for i in range(len(coeff_set)):
+# #     est = np.dot(p1.T, coeff_set[i].T)
+# #     plt.plot(est - exp_data[i,:])
+# #     plt.show()
+#
+#
+# # X_train, X_test, y_train, y_test = train_test_split(p1.T, exp_data.T, test_size=0.34, random_state= 10)
+# # las = Lasso()
+# # las.fit(X_train, y_test)
+# # y_est = las.predict(X_test)
+# #
+# # print(las.coef_)
