5bce58e7ca10fb0f6d0ed9409508cc68ce898305
onionpork <yiruic@g.clemson.edu>
2020-02-22 14:35:14 -0500

    Update RSM.py

--- RSM.py
+++ RSM.py
@@ -11,16 +11,16 @@ def RSM(t, pop):
             a_s*S*(1-(R+S+M)/K) - e*S*R - f*S*M,
             a_m*M*(1-(R+S+M)/K) + e*S*R + f*S*M]
 
-tspan = np.linspace(0, 20, num=201)
-dt = 0.1
+tspan = np.linspace(0, 20, num=2001)
+dt = 0.01
 ini = [10, 10000, 0]
 sol = integrate.solve_ivp(RSM, [tspan[0], tspan[-1]], ini, method='RK45', t_eval=tspan)
 
-# plt.plot(sol.t, sol.y[0].T, label='R')
-# plt.plot(sol.t, sol.y[1].T, label='S')
-# plt.plot(sol.t, sol.y[2].T, label='M')
-# plt.legend()
-# plt.show()
+plt.plot(sol.t, sol.y[0].T, label='R')
+plt.plot(sol.t, sol.y[1].T, label='S')
+plt.plot(sol.t, sol.y[2].T, label='M')
+plt.legend()
+plt.show()
 # print(sol.y.shape)
 
 def data_aug(sol_):
@@ -34,14 +34,34 @@ def data_aug(sol_):
         for j in range(n, 2*n):
             sol_ = np.vstack((sol_, sol_[i]*sol_[j]))
     return sol_
-
+print('sep')
 p1 = data_aug(sol.y)
 exp_data = np.diff(sol.y, axis=1) /dt
 p1 = p1[:, 1:]
 
-X_train, X_test, y_train, y_test = train_test_split(p1.T, exp_data.T, test_size=0.34, random_state= 10)
-las = Lasso()
-las.fit(X_train, y_test)
-y_est = las.predict(X_test)
+# print(exp_data.shape)
+# print(p1.shape)
 
-print(las.coef_)
+coeff_set = []
+for i in range(len(exp_data)):
+    X_train, X_test, y_train, y_test = train_test_split(p1.T, np.expand_dims(exp_data[0,:], axis=0).T, test_size=0.1, random_state= 10)
+    las = Lasso(alpha=1, max_iter=10**5, tol=0.001)
+    las.fit(X_train, y_train)
+    y_est = las.predict(X_test)
+    coeff_set.append(las.coef_)
+
+# print(p1[0,:].T)
+# print(coeff_set[0])
+# print(np.dot(p1[0,:].T, coeff_set[0]))
+# for i in range(len(coeff_set)):
+#     est = np.dot(p1.T, coeff_set[i].T)
+#     plt.plot(est)
+#     plt.show()
+
+
+# X_train, X_test, y_train, y_test = train_test_split(p1.T, exp_data.T, test_size=0.34, random_state= 10)
+# las = Lasso()
+# las.fit(X_train, y_test)
+# y_est = las.predict(X_test)
+#
+# print(las.coef_)
