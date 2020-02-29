38a79c78a5172cd290986607ff6e5cbe2a974804
onionpork <yiruic@g.clemson.edu>
2020-02-29 11:09:21 -0500

    Update RSM.py

--- RSM.py
+++ RSM.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import integrate
 import csv
+from sklearn.linear_model import lasso_path
 from scipy import optimize
 from sklearn.linear_model import Lasso
 from sklearn.model_selection import train_test_split
@@ -18,12 +19,6 @@ dt = 0.01
 ini = [10, 10000, 0]
 sol = integrate.solve_ivp(RSM, [tspan[0], tspan[-1]], ini, method='RK45', t_eval=tspan)
 
-# plt.plot(sol.t, sol.y[0].T, label='R')
-# plt.plot(sol.t, sol.y[1].T, label='S')
-# plt.plot(sol.t, sol.y[2].T, label='M')
-# plt.legend()
-# plt.show()
-# print(sol.y.shape)
 
 def data_aug(sol_):
     n = len(sol_)
@@ -40,37 +35,25 @@ p1 = data_aug(sol.y)
 exp_data = np.diff(sol.y, axis=1) /dt
 p1 = p1[:, 1:]
 
-#coeff_initial = np.dot(np.linalg.pinv(p1.T), exp_data.T)
 
 theta = p1.T
 exp_data = exp_data.T
 coeff_ = np.dot(np.linalg.pinv(theta), exp_data)
 
-for k in range(1000):
-    small_idx = (abs(coeff_) < 5e-7)
-    big_idx = (abs(coeff_) >= 5e-7)
-    coeff_[small_idx] = 0
-    for i in range(len(exp_data[0])):
-        #big_idx = np.bitwise_not([small_idx[:,i]]).T
-        coeff_[big_idx[:,i],i] = np.dot(np.linalg.pinv(theta[:,big_idx[:,i]]), exp_data[:, i])
+
+def sparsifyDynamics(theta, exp_data, lambda1):
+    for k in range(1000):
+        small_idx = (abs(coeff_) < lambda1)
+        big_idx = (abs(coeff_) >= lambda1)
+        coeff_[small_idx] = 0
+        for i in range(len(exp_data[0])):
+            coeff_[big_idx[:, i], i] = np.dot(np.linalg.pinv(theta[:, big_idx[:, i]]), exp_data[:, i])
+    return coeff_
+
+coeff_ = sparsifyDynamics(theta, exp_data, 5e-7)
+
+
 print(coeff_)
-# def sparsifyDynamics(p1, exp_data, lambda, n):
-#     theta = p1.T
-#     exp_data = exp_data.T
-#     coeff_ = np.dot(np.linalg.pinv(p1.T), exp_data.T)
-#
-#     for k in range(50):
-#         small_idx = abs(coeff_) < lambda
-#         coeff_[small_idx] = 0
-#         for i in range(len(exp_data)):
-#         big_idx != small_idx[:,i]
-#         coeff_[big_idx, i] = np.dot(np.linalg.pinv(theta[:,big_idx]), exp_data[:,idx])
-#     return coeff_
-
-#coeff_ = sparsifyDynamics(p1, exp_data, 5e-7, 3)
-
-
-#print(coeff_)
 est = np.dot(p1.T, coeff_)
 #
 plt.plot(est[:,0], '-*', lw=3, color='salmon', label ='est_dR')
@@ -88,34 +71,4 @@ with open(csvfile, "w") as output2:
     writer = csv.writer(output2, lineterminator='\n')
     writer.writerows(coeff_)
 
-# print(exp_data.shape)
-# print(p1.shape)
 
-# def fun(coeff_, p1, exp_data):
-#     return np.dot(p1.T, coeff_) - exp_data.T
-#
-# coeff_guess = np.array([np.random.rand(18)]).T
-# print(coeff_guess.shape)
-# res_log = optimize.least_squares(fun, coeff_guess, loss='cauchy', f_scale=0.1,
-#                                  args=(p1, exp_data))
-# # coeff_set = []
-# # for i in range(len(exp_data)):
-# #     X_train, X_test, y_train, y_test = train_test_split(p1.T, np.expand_dims(exp_data[i,:], axis=0).T, test_size=0.1)
-# #     las = Lasso(alpha=1, max_iter=10**5, tol=0.001)
-# #     las.fit(X_train, y_train)
-# #     y_est = las.predict(X_test)
-# #     coeff_set.append(las.coef_)
-#
-#
-# # for i in range(len(coeff_set)):
-# #     est = np.dot(p1.T, coeff_set[i].T)
-# #     plt.plot(est - exp_data[i,:])
-# #     plt.show()
-#
-#
-# # X_train, X_test, y_train, y_test = train_test_split(p1.T, exp_data.T, test_size=0.34, random_state= 10)
-# # las = Lasso()
-# # las.fit(X_train, y_test)
-# # y_est = las.predict(X_test)
-# #
-# # print(las.coef_)
