2d1db95ae715bff65f11191662d7d863c91423fb
onionpork <yiruic@g.clemson.edu>
2020-03-07 14:55:34 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -42,3 +42,54 @@ class utils:
         
         return Theta,descr
     ##
+
+    ##ADM algrithm
+    def soft_thresholding(X, lambda_):
+        temp_compare = X.T.copy()
+        for i in range(len(X)):
+            if abs(X[i]) - lambda_ < 0:
+                temp_compare[:, [i]] = 0
+            else:
+                temp_compare[:, [i]] = abs(X[i]) - lambda_
+        # tmp_compare = X[np.where(abs(X) - lambda_ > 0)]
+        # tmp_compare = np.expand_dims(tmp_compare, axis =1)
+        print(lambda_)
+        return np.multiply(np.sign(X), temp_compare.T)
+
+    def ADM(lib_null, q_init, lambda_, MaxIter, tol):
+        q = q_init.copy()
+        for i in range(MaxIter):
+            q_old = q.copy()
+            x = soft_thresholding(lib_null @ q_init, lambda_)
+            temp_ = lib_null.T @ x
+            q = temp_ / np.linalg.norm(temp_, 2)
+            res_q = np.linalg.norm(q_old - q, 2)
+
+            if res_q <= tol:
+                return q
+
+
+    def ADMinitvary(lib_null, lambda_, MaxIter, tol, pflag):
+        lib_null_norm = lib_null.copy()
+        for i in range(len(lib_null[0])):
+            lib_null_norm[:, i] = lib_null[:, i] / lib_null[:, i].mean()
+
+        q_final = np.empty_like(lib_null.T)
+        out = np.zeros((len(lib_null), len(lib_null)))
+        nzeros = np.zeros((1, len(lib_null)))
+        for i in range(len(lib_null_norm)):
+            q_ini = lib_null_norm[[i], :].T
+            temp_q = ADM(lib_null, q_ini, lambda_, MaxIter, tol)
+            q_final[:, [i]] = temp_q
+            temp_out = lib_null @ temp_q
+            out[:, [i]] = temp_out
+            nzeros_temp = sum(list((abs(temp_out) < lambda_)))
+            nzeros[:, [i]] = float(nzeros_temp)
+
+        idx_sparse = np.where(nzeros == max(np.squeeze(nzeros)))[0]
+        ind_lib = np.where(abs(out[:, idx_sparse[0]]) >= lambda_)[0]
+        Xi = out[:, idx_sparse[0]]
+        small_idx = np.where(abs(out[:, idx_sparse[0]]) < lambda_)[0]
+        Xi[small_idx] = 0
+        numterms = len(ind_lib)
+        return ind_lib, Xi, numterms
