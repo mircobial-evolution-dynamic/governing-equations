188e6e2f658c295120614d82d8db7c2a69185149
onionpork <yiruic@g.clemson.edu>
2020-03-07 14:55:32 -0500

    Update MM.py

--- MM.py
+++ MM.py
@@ -15,13 +15,23 @@ def MMKinetics(t, x):
     Vmax, Km, k = 1.5, 0.3, 0.6
     return k - np.divide(Vmax*x, (Km+x))
 
+def newmm(t,x):
+    k1, k2, k3, k4 = 0.1295, -0.6474, 0.2158, 0.7194
+    return np.divide((k1 + k2*x), (k3 + k4*x))
+
+
 
 tspan = np.linspace(0.01, 4, num=400)
 dt = 0.01
 ini = [0.5]
 sol = integrate.solve_ivp(MMKinetics, [tspan[0], tspan[-1]], ini, method='RK45', t_eval=tspan)
+sol2 = integrate.solve_ivp(newmm, [tspan[0], tspan[-1]], ini, method='RK45', t_eval=tspan)
+plt.plot(sol.y[0])
+plt.plot(sol2.y[0])
+plt.show()
 
 sol_dx = MMKinetics(sol.t, sol.y)
+plt.show()
 exp_data = sol_dx
 def data_aug(sol_):
     n = len(sol_)
@@ -47,7 +57,6 @@ def data_derivative(sol_,d_sol_):
 
 term_lib = data_aug(sol.y)
 term_lib = data_derivative(term_lib, sol_dx).T
-lib_null = linalg.null_space(term_lib)
 
 
 def soft_thresholding(X, lambda_):
@@ -59,7 +68,6 @@ def soft_thresholding(X, lambda_):
             temp_compare[:, [i]] = abs(X[i]) - lambda_
     # tmp_compare = X[np.where(abs(X) - lambda_ > 0)]
     # tmp_compare = np.expand_dims(tmp_compare, axis =1)
-    print(lambda_)
     return np.multiply(np.sign(X), temp_compare.T)
 
 
@@ -92,7 +100,6 @@ def ADMinitvary(lib_null, lambda_, MaxIter, tol, pflag):
         out[:,[i]] = temp_out
         nzeros_temp = sum(list((abs(temp_out) < lambda_)))
         nzeros[:,[i]] = float(nzeros_temp)
-
     idx_sparse = np.where(nzeros == max(np.squeeze(nzeros)))[0]
     ind_lib = np.where(abs(out[:, idx_sparse[0]]) >= lambda_)[0]
     Xi = out[:, idx_sparse[0]]
@@ -101,32 +108,37 @@ def ADMinitvary(lib_null, lambda_, MaxIter, tol, pflag):
     numterms = len(ind_lib)
     return ind_lib, Xi, numterms
 
-num = 1
-lambda_ = 1e-8
-MaxIter = 10000
-tol = 1e-5
-pflag = 1
-dic_lib = {}
-dic_Xi = {}
-dic_num = {}
-dic_error = {}
-dic_lambda = {}
-ii = 0
-while num > 0:
-    temp_ind_lib, temp_Xi, temp_numterms = ADMinitvary(lib_null, lambda_, MaxIter, tol, pflag)
-    dic_lib[ii] = temp_ind_lib
-    dic_Xi[ii] = temp_Xi
-    dic_num[ii] = temp_numterms
+def ADMpareto(term_lib, tol, pflag):
+    lib_null = linalg.null_space(term_lib)
+    num = 1
+    lambda_ = 1e-8
+    MaxIter = 10000
+    dic_lib = {}
+    dic_Xi = {}
+    dic_num = {}
+    dic_error = {}
+    dic_lambda = {}
+    ii = 0
+    while num > 0:
+        temp_ind_lib, temp_Xi, temp_numterms = ADMinitvary(lib_null, lambda_, MaxIter, tol, pflag)
+        dic_lib[ii] = temp_ind_lib
+        dic_Xi[ii] = temp_Xi
+        dic_num[ii] = temp_numterms
 
-    error_temp = sum(np.matmul(term_lib, dic_Xi[ii]))
-    dic_error[ii] = error_temp
-    dic_lambda[ii] = lambda_
-    lambda_ *= 2
-    num = dic_num[ii]
-    ii += 1
-    if lambda_ > 0.5:
-        break
+        error_temp = sum(np.matmul(term_lib, dic_Xi[ii]))
+        dic_error[ii] = error_temp
+        dic_lambda[ii] = lambda_
+        lambda_ *= 2
+        num = dic_num[ii]
+        ii += 1
+        if lambda_ > 0.5:
+            break
 
+    return dic_Xi, dic_lib, dic_lambda, dic_num, dic_error
+
+
+tol, pflag = 1e-5,1
+dic_Xi, dic_lib, dic_lambda, dic_num, dic_error = ADMpareto(term_lib,tol, pflag)
 lambda_vec = list(dic_lambda.values())
 terms_vec = list(dic_num.values())
 err_vec = list(dic_error.values())
@@ -143,4 +155,16 @@ plt.xlabel("Number of terms")
 plt.ylabel("Error (log)")
 plt.show()
 
+def new_MMKinetics(term_lib, dic_Xi, n):
+    half_count = int(term_lib.shape[1] /2)
+    total_xi = len(dic_Xi)
+    #term_coff = dic_Xi[total_xi - n]
+    term_coff = [0.18, -0.9, 0, 0, 0.3, 1, 0, 0]
+    print(term_coff)
+    return -np.divide(np.matmul(term_lib[:,:half_count], term_coff[:half_count]),
+                      np.matmul(term_lib[:,half_count:], term_coff[half_count:]))
 
+res2 = new_MMKinetics(term_lib, dic_Xi, 4)
+plt.plot(sol_dx[0])
+plt.plot(res2)
+plt.show()
